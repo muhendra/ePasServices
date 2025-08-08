5fa7a22827aeb43a28161b2ede1ace37ba7ba445
Bonaventura Pinandito <bonaventura.pinandito@dkatalis.com>
2025-08-08 12:40:36 +0700

    feat: add for mobile SPBU and notification

---
+++ ePasServices/Services/Interfaces/INotificationService.cs
@@ -0,0 +1,12 @@
+﻿using ePasServices.Models;
+using ePasServices.ViewModels;
+
+namespace ePasServices.Services.Interfaces
+{
+    public interface INotificationService
+    {
+        Task<(List<NotificationListItemViewModel> Data, int Total)> GetNotificationListAsync(int page, int limit, string userId);
+
+        Task<bool> UpdateNotificationStatusAsync(string id, string status, string userId);
+    }
+}
---
+++ ePasServices/ViewModels/NotificationListItemViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ePasServices.ViewModels
+{
+    public class NotificationListItemViewModel
+    {
+        public string Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+    }
+}
